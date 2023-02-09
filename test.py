113481fe1e092d4090dd76f008cbf69b076af827
Jeff H <jeffrey92@gmail.com>
2023-02-09 13:42:59 -0800

    re-arrange test.py
    still pretty ugly but serves as a quick boilerplate to test
    various features

--- test.py
+++ test.py
@@ -4,17 +4,61 @@ import blufi
 import time
 import sys
 
+################################################################################
+# Options for misc. tests
+################################################################################
+# Enable/Disable negotiateSecurity
+TEST_SECURITY = True
+
+# Enable/Disable getVersion
+TEST_VERSION = True
+
+# Enable/Disable get SSID list
+TEST_SCAN = False
+
+# Enable/Disable send SSID/Pass
+TEST_POST_WIFI = False
+TEST_POST_WIFI_CREDS = {
+    'ssid': 'yourssid',
+    'pass': 'yourpass'
+}
+
+# Test behavior when disabling rx notifications
+TEST_NOTIFY = False
+
+################################################################################
+
+# Create client instance
 client = blufi.BlufiClient()
 
+# connect to BLUFI_DEVICE
+# NOTE: atexit is used internally to send disconnect before script exits
 client.connectByName('BLUFI_DEVICE')
-# NOTE: See README.md about MTU
-client.setPostPackageLengthLimit(256)
-client.negotiateSecurity()
-client.requestVersion()
-print('Version: ', client.getVersion())
 
-testNotify = False
-if testNotify:
+# Cap pkt size. See README.md about MTU
+client.setPostPackageLengthLimit(256)
+
+if TEST_SECURITY:
+    client.negotiateSecurity()
+
+if TEST_VERSION:
+    client.requestVersion()
+    print('Version: ', client.getVersion())
+
+if TEST_SCAN:
+    # Reset STA state in case its attempting to connect
+    client.postDeviceMode(blufi.OP_MODE_NULL)
+    client.postDeviceMode(blufi.OP_MODE_STA)
+    client.requestDeviceScan()
+    print('SSIDs:')
+    for item in client.getSSIDList():
+        print(item)
+
+if TEST_POST_WIFI:
+    client.postDeviceMode(blufi.OP_MODE_STA)
+    client.postStaWifiInfo(TEST_POST_WIFI_CREDS)
+
+if TEST_NOTIFY:
     client.stopNotify()
     client.postDeviceMode(blufi.OP_MODE_NULL)
     client.postDeviceMode(blufi.OP_MODE_NULL)
@@ -25,17 +69,4 @@ if testNotify:
 
     sys.exit(0)
 
-# Reset STA state in case its attempting to connect
-client.postDeviceMode(blufi.OP_MODE_NULL)
-client.postDeviceMode(blufi.OP_MODE_STA)
-client.requestDeviceScan()
-print('SSIDs:')
-for item in client.getSSIDList():
-    print(item)
-
-# client.postDeviceMode(blufi.OP_MODE_STA)
-# params = {
-#     'ssid': 'yourssid',
-#     'pass': 'yourpass'
-# }
-# client.postStaWifiInfo(params)
+print('Exiting')
