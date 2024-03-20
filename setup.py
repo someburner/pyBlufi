4ebbcc6895daa30e5c251a13a67ea5790f5ebe3e
Michael Bach Kristensen <tacoz666+pyBlufi@gmail.com>
2024-03-20 11:43:30 +0100

    Setup - Fix find packages + Added description + Addedd requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,10 @@ from setuptools import setup, find_packages
 setup(
     name='pyBlufi',
     version='0.0.1',
-    packages=find_packages('blufi'),
+    description="Python utility to interface with esp32 Blufi component.",
+    packages=find_packages(),
+    install_requires=[
+        'cryptography==38.0.4',
+        'bleak==0.21.1'
+    ],
 )
